46812af6d5f3bc7c8f70913eda23ce2a38ab4db0
Nick Fedor <nick@nickfedor.com>
2025-07-18 18:04:54 -0700

    chore(structure): move tplprev tool source to /tools/tplprev and update build script

--- scripts/build-tplprev.sh
+++ scripts/build-tplprev.sh
@@ -4,4 +4,4 @@ cd $(git rev-parse --show-toplevel)
 
 cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" ./docs/assets/
 
-GOARCH=wasm GOOS=js go build -o ./docs/assets/tplprev.wasm ./tplprev
+GOARCH=wasm GOOS=js go build -o ./docs/assets/tplprev.wasm ./tools/tplprev
