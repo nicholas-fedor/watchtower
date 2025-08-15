7254105b78452f8f3789e21563606282b70db99d
Nick Fedor <nick@nickfedor.com>
2025-08-15 14:40:56 -0700

    ci(scripts): update built-tplprev script
    - Update expected location of local wasm file
    - Add comments to document the script's steps

--- scripts/build-tplprev.sh
+++ scripts/build-tplprev.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Navigate to the repository root
 cd $(git rev-parse --show-toplevel)
 
-cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" ./docs/assets/
+# Copy wasm_exec.js from GOROOT/lib/wasm to docs/assets/
+cp "$(go env GOROOT)/lib/wasm/wasm_exec.js" ./docs/assets/
 
+# Build the tplprev WebAssembly binary
 GOARCH=wasm GOOS=js go build -o ./docs/assets/tplprev.wasm ./tools/tplprev
